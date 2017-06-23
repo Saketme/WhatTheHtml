0989e6846e4d28d9631c1d52210e84890f1150f3
Saket Narayan <saket@saket.me>
2017-06-23 17:38:34 +0530

    Sup world?

---
+++ app/src/main/java/is/uncommon/whatthehtml/MainActivity.java
@@ -0,0 +1,23 @@
+package is.uncommon.whatthehtml;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+  @BindView(R.id.text) TextView textView;
+  @BindString(R.string.what_the_html) String htmlString;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
+
+    textView.setText(R.string.what_the_html);
+  }
+}
